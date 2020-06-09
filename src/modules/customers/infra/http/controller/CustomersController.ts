c6c4b5ab825de4a2f8948d7b489344a71683c40d
Rodrigo Coelho <rodrigo.coelho@hotmail.com.br>
2020-06-09 10:19:32 -0400

    Challenge finished

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,9 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const customerService = container.resolve(CreateCustomerService);
+    const { name, email } = request.body;
+    const user = await customerService.execute({ name, email });
+    return response.status(200).json(user);
   }
 }
--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,20 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findUser = await this.customersRepository.findByEmail(email);
+    if (findUser) {
+      throw new AppError('Email already exists');
+    }
+
+    const user = await this.customersRepository.create({ email, name });
+
+    return user;
   }
 }
 
